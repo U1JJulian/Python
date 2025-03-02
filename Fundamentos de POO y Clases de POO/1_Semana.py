054d900d6e856882fba85a4cd381de5e6070f690
U1JJulian <03jjulianu1@gmail.com>
2025-03-02 08:49:05 -0600

    Primer avance del proyecto

---
+++ Fundamentos de POO y Clases de POO/1_Semana.py
@@ -0,0 +1,10 @@
+class Producto():
+    def __init__(self, Nombre, Precio, Stock):
+        self.Nombre = Nombre
+        self.Precio = Precio
+        self.Stock = Stock
+    def __str__(self):
+        return f" \n Nombre del producto: {self.Nombre} \n Precio del producto: {self.Precio} \n Cantidad de productos disponibles: {self.Stock}"
+
+miProducto = Producto("Samsung s18", 5000, 15)
+print(miProducto)
